7ef0421e58bb9757aee4d0e95d4dee9b0f32276d
Raja <123.raja.sriram@gmail.com>
2020-12-05 22:38:15 -0500

    first commit

---
+++ src/test/java/com/w2a/utilities/ExtentManager.java
@@ -0,0 +1,33 @@
+package com.w2a.utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentManager {
+	
+	private static ExtentReports extent;
+	
+	
+	
+
+    public static ExtentReports createInstance(String fileName) {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(fileName);
+       
+        
+        htmlReporter.config().setTheme(Theme.DARK);
+        htmlReporter.config().setDocumentTitle(fileName);
+        htmlReporter.config().setEncoding("utf-8");
+        htmlReporter.config().setReportName(fileName);
+        
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Automation Tester", "Raja");
+        extent.setSystemInfo("Organization", "Test");
+        extent.setSystemInfo("Build no", "1000");
+        
+        
+        return extent;
+    }
+
+}
